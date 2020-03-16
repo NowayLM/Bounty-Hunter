313cd2a9c2f5808fcf6a4356ca3e1c7e287e5104
DouleurLicence <57877464+DouleurLicence@users.noreply.github.com>
2020-03-16 16:26:44 +0100

    AudioManager added. We can now implement every sound and change the volume, the pitch and add a loop to the song. The buttons are now transparent and the colors are linked to the theme of the menu's sprite. Added every font windows has on a new folder called font.

---
+++ BountyHunter/Assets/Scripts/Sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound 
+{
+    public string name;
+
+    public AudioClip clip;
+
+    // Sliders for the volume and the pitch
+    [Range(0f,1f)]
+    public float volume;
+    [Range(0.1f,3f)]
+    public float pitch;
+
+    // Toggle that loops the sound
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
