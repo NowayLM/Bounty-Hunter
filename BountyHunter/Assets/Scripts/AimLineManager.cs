07c703adf42a54789975a40c97effc494879110b
NowayLM <60277147+NowayLM@users.noreply.github.com>
2020-04-20 17:03:17 +0200

    Non Working Aim

---
+++ BountyHunter/Assets/Scripts/AimLineManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimLineManager : MonoBehaviour
+{
+
+    protected LineRenderer aim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        aim = GetComponent<LineRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        var points = new Vector3[1];
+        points[0] = GameObject.Find("Player").transform.position;
+        points[1] = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        aim.SetPositions(points);
+    }
+}
