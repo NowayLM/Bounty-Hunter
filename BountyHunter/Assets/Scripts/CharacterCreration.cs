f6fa83e8c501bf87455b61d61a0857335a433235
DouleurLicence <57877464+DouleurLicence@users.noreply.github.com>
2020-04-20 15:02:17 +0200

    scenes linked

--- BountyHunter/Assets/Scripts/CharacterCreration.cs
+++ BountyHunter/Assets/Scripts/CharacterCreration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class CharacterCreration : MonoBehaviour
 {
--- BountyHunter/Assets/Scripts/MainMenu.cs
+++ BountyHunter/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        Camera.DontDestroyOnLoad(gameObject);
         SceneManager.LoadScene(1);
     }
 
