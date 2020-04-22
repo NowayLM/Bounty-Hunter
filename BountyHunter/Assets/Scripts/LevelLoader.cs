a0309ab6ed1747d7b6d571fcbfed4c690da5e72c
DouleurLicence <57877464+DouleurLicence@users.noreply.github.com>
2020-04-22 20:05:28 +0200

    Menu finished bande de fdp par contre il manque 3 modèles et lancelot tu dois faire en sorte de récupérer l'index pour choisir le personnage

--- BountyHunter/Assets/Scripts/LevelLoader.cs
+++ BountyHunter/Assets/Scripts/LevelLoader.cs
@@ -1,11 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
 
@@ -23,11 +21,5 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("QUIT!");
-        Application.Quit();
     }
 }
