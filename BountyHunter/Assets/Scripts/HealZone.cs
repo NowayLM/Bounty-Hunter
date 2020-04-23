5885535bfc86d2079f26ef685fcfb86f6263cc06
NowayLM <60277147+NowayLM@users.noreply.github.com>
2020-04-23 14:41:37 +0200

    dsl j'étais sur master

---
+++ BountyHunter/Assets/Scripts/HealZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+class HealZone : MonoBehaviour 
+{
+    void Update()
+    {
+        Console.WriteLine("TEst");
+    }
+}
